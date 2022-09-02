ddb24139bf5baeeefc6d66bbc6c960ec157bbb43
Deepak <dgandhi570@gmail.com>
2022-09-02 14:48:32 +0530

    initial

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/create'
+  get 'comments/new'
+  get 'comments/destroy'
+
+  devise_for :users
+  resources :posts do
+    resources :comments
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+   root "posts#index"
+end
